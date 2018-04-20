68c189bcce1f674a13ad4bd87b2fc8c732f1f318
Jonah Wooten <38252481+Jonah-Wooten@users.noreply.github.com>
2018-04-20 08:23:12 -0400

    Update Time.java

--- src/Time.java
+++ src/Time.java
@@ -1,3 +1,4 @@
+//The imports needed to run the code
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Scanner;
@@ -9,37 +10,47 @@ public class Time {
 		// Input Scanner
 		Scanner input = new Scanner(System.in);
 
-		// User inputs in a date
+		// User inputs their first year
 		System.out.println("Enter 4 numbers for Year:");
 		int year1 = input.nextInt();
-
+		
+		//User inputs their first month
 		System.out.println("Enter 2 numbers for Month:");
 		int month1 = input.nextInt();
-
+		
+		//User inputs their first day
 		System.out.println("Enter 2 numbers for Day:");
 		int day1 = input.nextInt();
-
+		
+		//Made the user's input for the first date into an integer
 		LocalDate date1 = LocalDate.of(year1, month1, day1);
-
+		
+		//User inputs their second year
 		System.out.println("Enter 4 numbers for Year:");
 		int year2 = input.nextInt();
-
+		
+		//User inputs their second month
 		System.out.println("Enter 2 numbers for Month:");
 		int month2 = input.nextInt();
-
+		
+		//User inputs their second day
 		System.out.println("Enter 2 numbers for Day:");
 		int day2 = input.nextInt();
-
+ 		
+		//Made the user's input for the second day into an integer
 		LocalDate date2 = LocalDate.of(year2, month2, day2);
-
+		
+		//The code that finds the difference between the two date
 		Period diff = Period.between(date1, date2);
-
+		
+		//Making the difference and integer in Years/Months/Days
 		int Difference1 = diff.getYears();
 		int Difference2 = diff.getMonths();
 		int Difference3 = diff.getDays();
-
-		System.out.println(Difference1);
-		System.out.println(Difference2);
-		System.out.println(Difference3);
+		
+		//The Difference between the 2 Dates in YYYY/MM/DD
+		System.out.println(Difference1 + "-Years");
+		System.out.println(Difference2 + "-Months");
+		System.out.println(Difference3 + "-Days");
 	}
 }
